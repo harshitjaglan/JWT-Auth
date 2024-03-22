c33c0b92d12a515e9bba624851a98a060be4449f
harshitjaglan <116933487+harshitjaglan@users.noreply.github.com>
2024-03-22 01:08:58 -0700

    db

---
+++ db/index.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://harshitjaglan78:H%40905090j@cluster0.sh3mmu5.mongodb.net/Sell-Course1"
+);
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+  // Schema definition here
+  username: String,
+  password: String,
+});
+
+const UserSchema = new mongoose.Schema({
+  // Schema definition here
+  username: String,
+  password: String,
+  purchasedCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId, // this will make sure
+      // we use 24-character hexadecimal strings.
+      ref: "Course",
+    },
+  ],
+});
+
+const CourseSchema = new mongoose.Schema({
+  // Schema definition here
+  title: String,
+  description: String,
+  imageLink: String,
+  price: Number,
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
+
+module.exports = {
+  Admin,
+  User,
+  Course,
+};
